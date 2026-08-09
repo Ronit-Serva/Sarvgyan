2f6ab47e75d2a9a666de1c1c641481409b97ea6b
Ronit Serva <ronitkumar.serva@gmail.com>
2026-08-09 21:17:54 +0530

    Added a requirements.txt to easily get the dependencies installed

--- test.py
+++
@@ -1,6 +0,0 @@
-from tavily import TavilyClient
-
-tavily_client = TavilyClient(api_key="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")
-response = tavily_client.search("Who is Leo Messi?", include_answer=True)
-
-print('Answer:', response['answer'])
